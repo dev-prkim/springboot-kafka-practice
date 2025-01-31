0fe8c9bae57d2bb2d099ef8135f9e241f473feb9
dev-prkim <prkim429@gmail.com>
2025-01-31 16:53:17 +0900

    project init

---
+++ src/main/java/com/example/spring_kafka_practice/SpringKafkaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_kafka_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_kafka_practice/SpringKafkaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_kafka_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
